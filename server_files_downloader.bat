b4ab8557b6edddf68cb595dbf6796d86cc8865bf
Francesco Milano <28491164+framilano@users.noreply.github.com>
2025-12-30 11:56:00 +0100

    Modify server_files_downloader.bat for InstallPath setup
    Updated the script to set the InstallPath and adjusted the directory structure for SteamCMD.

--- server_files_downloader.bat
+++ server_files_downloader.bat
@@ -3,6 +3,13 @@ set "SteamcmdUrl=https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip"
 set "T7XUrl=https://master.bo3.eu/t7x/t7x.exe"
 set "EZZBOIIIUrl=https://github.com/Ezz-lol/boiii-free/releases/latest/download/boiii.exe"
 
+::SEtting current folder as InstallPath for UnrankedServer
+set "InstallPath=%CD%"
+
+::SteamCMD doesn't want steamcmd and steamapps on the same level folder, for some reason, nesting steamcmd inside steamstuff
+mkdir steamstuff
+cd steamstuff
+
 ::Retrieves SteamCMD
 curl -s "%SteamcmdUrl%" -o steamcmd.zip
 mkdir steamcmd
@@ -11,14 +18,17 @@ cd steamcmd
 tar -xf steamcmd.zip
 
 ::Downloading server files
+echo %InstallPath%
 echo Login is required to download Black Ops 3 Unranked Server files
 set /p SteamUser=Steam Username: 
-steamcmd +force_install_dir "..\" +login %SteamUser% +app_update 545990 validate +quit
+steamcmd +force_install_dir %InstallPath% +login %SteamUser% +app_update 545990 validate +quit
 
 ::deleting unecessary folders
 cd ..
-rd /s /q steamapps
 rd /s /q steamcmd
+cd ..
+rd /s /q steamstuff
+rd /s /q steamapps
 
 ::deleting unecessary files
 cd UnrankedServer
